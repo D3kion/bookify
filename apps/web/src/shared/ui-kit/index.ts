7cd35c8ceac87a7a4b7a1400c812d0049f0bbc01
D3kion <dekion.dev@gmail.com>
2025-07-24 20:03:18 +0300

    feat(web): dashboard rework WIP

--- apps/web/src/shared/ui-kit/index.ts
+++ apps/web/src/shared/ui-kit/index.ts
@@ -1,9 +1,13 @@
 export * from "./lib/utils";
 
 export * from "./ui/button";
-export * from "./ui/input";
 export * from "./ui/label";
+export * from "./ui/input";
+export * from "./ui/select";
+export * from "./ui/toggle";
+export * from "./ui/toggle-group";
 
+export * from "./ui/chart";
 export * from "./ui/card";
 export * from "./ui/badge";
 export * from "./ui/progress";
